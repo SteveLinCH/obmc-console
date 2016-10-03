44b08fd69bfd620b0a143c573f594003dd21077e
Ken Lai <ken1029@gmail.com>
2016-10-03 11:21:44 +0800

    esc test

--- console-client.c
+++ console-client.c
@@ -44,7 +44,7 @@ struct console_client {
 	bool		newline;
 };
 
-static const uint8_t esc_str[] = { '~', '.' };
+static const uint8_t esc_str[] = { '~', '[' };
 
 static enum process_rc process_tty(struct console_client *client)
 {
